534be9a55e98d12ce033492161781159c6543faf
Victor Gabriel <victorgabriel.cipa@gmail.com>
2019-10-08 17:34:26 -0300

    Cria a view e a controller de Funcionarios

--- SistemaJobs/Funcionario.cs
+++ SistemaJobs/Funcionario.cs
@@ -11,18 +11,45 @@ namespace SistemaJobs
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Funcionario
     {
         public int IdFuncionario { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string Nome { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Sobrenome { get; set; }
+
+        [Required]
+        [StringLength(14, MinimumLength = 14)]
         public string CPF { get; set; }
+
+        [Required]
+        [StringLength(11, MinimumLength = 11)]
         public string Telefone { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(2, MinimumLength = 2)]
         public string Estado { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Cidade { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string Usuario { get; set; }
+
+        [Required]
+        [StringLength(12, MinimumLength = 4)]
         public string Senha { get; set; }
     }
 }
