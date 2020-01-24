397fcf5baecc80b02fd46c560f8e4739cc348c32
Victor Gabriel <victorgabriel.cipa@gmail.com>
2020-01-24 17:13:24 -0300

    Implementando a pagina de busca de emprego

---
+++ SistemaJobs/Controllers/VagasEmpregoController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SistemaJobs;
+using PagedList;
+
+namespace SistemaJobs.Controllers
+{
+    public class VagasEmpregoController : Controller
+    {
+        private HiredItEntities db = new HiredItEntities();
+
+        // GET: VagasEmprego
+        public ViewResult Index(string currentFilter, string searchString, int? page)
+        {
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+            var vagas = from s in db.Empresa select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                vagas = vagas.Where(s => s.Nome.ToUpper().Contains(searchString.ToUpper()));
+            }
+
+            vagas = vagas.OrderByDescending(s => s.Nome);
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(vagas.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: VagasEmprego/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empresa empresa = db.Empresa.Find(id);
+            if (empresa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresa);
+        }
+
+        // GET: VagasEmprego/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VagasEmprego/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdEmpresa,Nome,CNPJ,Telefone,Email,Estado,Cidade,Usuario,Senha")] Empresa empresa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Empresa.Add(empresa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(empresa);
+        }
+
+        // GET: VagasEmprego/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empresa empresa = db.Empresa.Find(id);
+            if (empresa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresa);
+        }
+
+        // POST: VagasEmprego/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdEmpresa,Nome,CNPJ,Telefone,Email,Estado,Cidade,Usuario,Senha")] Empresa empresa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empresa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(empresa);
+        }
+
+        // GET: VagasEmprego/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empresa empresa = db.Empresa.Find(id);
+            if (empresa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empresa);
+        }
+
+        // POST: VagasEmprego/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empresa empresa = db.Empresa.Find(id);
+            db.Empresa.Remove(empresa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
